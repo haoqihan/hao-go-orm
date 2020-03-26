ec66d7b13c03354f0285fcecb26f7a4e96108c18
haoqihan <2263310007@qq.com>
2020-03-26 15:04:13 +0800

    数据库迁移

--- hao-orm_test.go
+++ hao-orm_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	_ "github.com/mattn/go-sqlite3"
 	"hao-go-orm/session"
+	"reflect"
 	"testing"
 )
 
@@ -17,7 +18,7 @@ func OpenDB(t *testing.T) *Engine {
 }
 
 type User struct {
-	Name string `geeorm:"PRIMARY KEY"`
+	Name string `horm:"PRIMARY KEY"`
 	Age  int
 }
 
@@ -30,6 +31,23 @@ func TestEngine_Transaction(t *testing.T) {
 	})
 }
 
+
+
+func TestEngine_Migrate(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text PRIMARY KEY, XXX integer);").Exec()
+	_, _ = s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam").Exec()
+	engine.Migrate(&User{})
+
+	rows, _ := s.Raw("SELECT * FROM User").QueryRows()
+	columns, _ := rows.Columns()
+	if !reflect.DeepEqual(columns, []string{"Name", "Age"}) {
+		t.Fatal("Failed to migrate table User, got columns", columns)
+	}
+}
 
 func transactionRollback(t *testing.T) {
 	engine := OpenDB(t)
